d52099684eeff181cebe39501ce7cc41938d326f
mohammad Arbabi <muhammad.arbabi@gmail.com>
2023-02-10 23:30:43 +0330

    Add PagingSource for popular movies

---
+++ app/src/main/java/com/della/hassintmdbtask/data/model/movie/PopularMoviesPagingSource.kt
@@ -0,0 +1,30 @@
+package com.della.hassintmdbtask.data.model.movie
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.della.hassintmdbtask.data.api.ApiService
+import com.della.hassintmdbtask.data.model.movie.popular.Movie
+
+class PopularMoviesPagingSource(private val apiService: ApiService) : PagingSource<Int,Movie>() {
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        return try {
+            val page = params.key ?: 1
+            val response = apiService.getPopularMovies(page = page)
+
+            LoadResult.Page(
+                data = response.movies,
+                prevKey = if (page == 1) null else page.minus(1),
+                nextKey = if (response.movies.isEmpty()) null else page.plus(1),
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
