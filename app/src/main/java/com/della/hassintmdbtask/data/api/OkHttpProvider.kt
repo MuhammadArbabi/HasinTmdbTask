a72d938d96b167af93efc82d53605537fd6a0f47
mohammad Arbabi <muhammad.arbabi@gmail.com>
2023-02-10 20:49:55 +0330

    Add OkHttpProvider to add api_key as a query parameter to all requests and log requests and responses

--- app/src/main/java/com/della/hassintmdbtask/data/api/OkHttpProvider.kt
+++ app/src/main/java/com/della/hassintmdbtask/data/api/OkHttpProvider.kt
@@ -1,11 +1,27 @@
 package com.della.hassintmdbtask.data.api
 
-import android.util.Log
 import com.della.hassintmdbtask.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
 
+/**
+ *The class OkHttpProvider is an object that provides a singleton instance of OkHttpClient.
+ *
+ * Here's what the code does :
+ *
+ * 1. Creates an instance of HttpLoggingInterceptor, which is used to log HTTP network requests and response.
+ *
+ * 2. Builds the OkHttpClient instance using the OkHttpClient.Builder class. The builder is configured with two interceptors:
+ *
+ *
+ *      a. The first interceptor adds the API key as a query parameter to all requests. The API key is taken from the BuildConfig.API_KEY field.
+ *
+ *
+ *      b. The second interceptor is the okHttpLogger instance, which logs network requests and responses.
+
+
+ */
 object OkHttpProvider {
     val instance : OkHttpClient
 
