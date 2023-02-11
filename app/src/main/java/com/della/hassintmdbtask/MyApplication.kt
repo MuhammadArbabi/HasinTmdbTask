8cdbc8a2e8e72460a744fdef4ce8259c47a01b4c
mohammad Arbabi <muhammad.arbabi@gmail.com>
2023-02-12 00:57:10 +0330

    Add HiltAndroidApp Annotation to MyApplication class Add new dependencies (coil, lifcycle-viewmodel-compose,...) to project

--- app/src/main/java/com/della/hassintmdbtask/MyApplication.kt
+++ app/src/main/java/com/della/hassintmdbtask/MyApplication.kt
@@ -2,12 +2,13 @@ package com.della.hassintmdbtask
 
 import android.app.Application
 import com.della.hassintmdbtask.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
         if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
