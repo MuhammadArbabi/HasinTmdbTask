2d743866a19d997cd40cf6523637a574e48f40df
mohammad Arbabi <muhammad.arbabi@gmail.com>
2023-02-14 02:18:06 +0330

    add Retry button for loading Errors

--- app/src/main/java/com/della/hassintmdbtask/compose/Common.kt
+++ app/src/main/java/com/della/hassintmdbtask/compose/Common.kt
@@ -1,10 +1,9 @@
 package com.della.hassintmdbtask.compose
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,26 +12,35 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingIndicator() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator(
-                modifier = Modifier.size(50.dp)
-            )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(50.dp)
+        )
     }
 }
+
 @Composable
-fun ErrorLoading() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+fun ErrorLoading(message: String, onRetryClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            color = MaterialTheme.colorScheme.error,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedButton(
+            onClick = { onRetryClick() }
         ) {
-            Text(
-                text = "Error Loading",
-                color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.size(50.dp)
-            )
+            Text(text = "Retry...",
+            style = MaterialTheme.typography.titleMedium)
+        }
     }
 }
 
